46429dcfe997191ef9d463626b118086515cf1eb
Nikhil Desai <Nikhil.Desai@talentica.com>
2023-03-27 08:39:31 +0530

    add benchmark contract

---
+++ template/benchmark-sample/contracts/benchmark-sample.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract BenchmarkSample {
+    string some_str;
+    int some_num;
+
+    constructor() {
+        some_str = "Default";
+        some_num = 0;
+    }
+
+    function set_some_num(int new_num) external {
+        some_num = new_num;
+    }
+
+    function get_some_num() external view returns (int) {
+        return some_num;
+    }
+
+    function set_some_str(string memory new_s) external {
+        some_str = new_s;
+    }
+
+    function get_some_str() external view returns (string memory) {
+        return some_str;
+    }
+}
